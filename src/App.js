0dbdc5c2c88fb4a4c113551c8dbeff277ef89797
Yazid J. Alaoui <yazid.ja@gmail.com>
2021-04-09 18:20:08 +0100

    Final Logic

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ class App extends React.Component {
     breakLength: 5,
     sessionLength: 25,
     label: "SESSION",
-    timeLeft: "25:00",
     running: false,
     startStop: "Start",
-    minutes: 25,
-    seconds: 0,
+    duration: [25, 0], // mins, secs
+    formatted: "25:00",
+    timeout: 0,
+    onBreak: false,
   };
 
   breakHandler(e) {
@@ -20,7 +21,7 @@ class App extends React.Component {
     if (value === "+" && breakLength < 60) {
       breakLength++;
     }
-    if (value === "-" && breakLength > 0) {
+    if (value === "-" && breakLength > 1) {
       breakLength--;
     }
     this.setState({ breakLength });
@@ -29,26 +30,84 @@ class App extends React.Component {
   sessionHandler(e) {
     let sessionLength = this.state.sessionLength;
     let value = e.target.innerHTML;
+    let duration = this.state.duration;
     if (value === "+" && sessionLength < 60) {
       sessionLength++;
     }
-    if (value === "-" && sessionLength > 0) {
+    if (value === "-" && sessionLength > 1) {
       sessionLength--;
     }
-    this.setState({ sessionLength });
+    duration[0] = sessionLength;
+    this.setState({ sessionLength, duration });
+    this.setPadding();
   }
 
+  durationHandler = () => {
+    let duration = [...this.state.duration];
+    let label = this.state.label;
+    let breakLength = this.state.breakLength;
+    let sessionLength = this.state.sessionLength;
+    let onBreak = this.state.onBreak;
+    if (duration[0] + duration[1] === 0) {
+      if (!onBreak) {
+        onBreak = true;
+        label = "ON BREAK";
+        duration[0] = breakLength;
+      } else {
+        onBreak = false;
+        label = "ON SESSION";
+        duration[0] = sessionLength;
+      }
+      document.getElementById("beep").play();
+    } else {
+      if (duration[1] === 0) {
+        duration[1] = 59;
+        duration[0] -= 1;
+      } else {
+        duration[1] -= 1;
+      }
+    }
+    this.setState({ duration, label, onBreak });
+    this.setPadding();
+  };
+
+  setPadding = () => {
+    let duration = [...this.state.duration];
+    let minutes,
+      seconds = 0;
+    // minutes padding
+    if (duration[0] < 10) {
+      minutes = "0" + duration[0];
+    } else {
+      minutes = duration[0];
+    }
+    // seconds padding
+    if (duration[1] < 10) {
+      seconds = "0" + duration[1];
+    } else {
+      seconds = duration[1];
+    }
+    let formatted = `${minutes}:${seconds}`;
+    this.setState({ formatted });
+  };
+
   startHandler = () => {
-    let label = "RUNNING";
     let running = true;
     let startStop = "STOP";
-    this.setState({ label, running, startStop });
-    // stopwatch = setInterval(timerHandler, 10);
+    let onBreak = this.state.onBreak;
+    let label = this.state.label;
+    if (!onBreak) {
+      label = "ON SESSION";
+    } else {
+      label = "ON BREAK";
+    }
+    let timeout = setInterval(this.durationHandler.bind(this), 1000);
+    this.setState({ label, running, startStop, timeout });
   };
 
   stopHandler = () => {
-    // clearInterval(stopwatch);
-    let label = "STOPPED";
+    clearInterval(this.state.timeout);
+    let label = "PAUSED";
     let running = false;
     let startStop = "START";
     this.setState({ label, running, startStop });
@@ -68,63 +127,96 @@ class App extends React.Component {
     let startStop = "START";
     let breakLength = 5;
     let sessionLength = 25;
-    this.setState({ label, running, startStop, breakLength, sessionLength });
-    // clearInterval(stopwatch);
-    // setText();
+    let duration = [25, 0];
+    let formatted = "25:00";
+    this.setState({
+      label,
+      running,
+      startStop,
+      breakLength,
+      sessionLength,
+      duration,
+      formatted,
+    });
+    clearInterval(this.state.timeout);
+    document.getElementById("beep").pause();
+    document.getElementById("beep").currentTime = 0;
   };
 
   render() {
     return (
-      <table>
-        <tbody>
-          <tr>
-            <td id="break-label" colSpan="3">
-              Break Length
-            </td>
-          </tr>
-          <tr>
-            <td id="break-decrement" onClick={this.breakHandler.bind(this)}>
-              +
-            </td>
-            <td id="break-length">{this.state.breakLength}</td>
-            <td id="break-increment" onClick={this.breakHandler.bind(this)}>
-              -
-            </td>
-          </tr>
-          <tr>
-            <td id="session-label" colSpan="3">
-              Session Length
-            </td>
-          </tr>
-          <tr>
-            <td id="session-decrement" onClick={this.sessionHandler.bind(this)}>
-              +
-            </td>
-            <td id="session-length">{this.state.sessionLength}</td>
-            <td id="session-increment" onClick={this.sessionHandler.bind(this)}>
-              -
-            </td>
-          </tr>
-          <tr>
-            <td id="timer-label" colSpan="3">
-              {this.state.label}
-            </td>
-          </tr>
-          <tr>
-            <td id="time-left" colSpan="3">
-              {this.state.timeLeft}
-            </td>
-          </tr>
-          <tr>
-            <td id="reset" onClick={this.reset}>
-              Reset
-            </td>
-            <td id="start_stop" colSpan="2" onClick={this.startStop}>
-              {this.state.startStop}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <audio
+          id="beep"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        />
+        <table>
+          <tbody>
+            <tr>
+              <td id="break-label" colSpan="3">
+                Break Length
+              </td>
+            </tr>
+            <tr>
+              <td
+                id="break-increment"
+                onClick={this.breakHandler.bind(this)}
+                className="interactive">
+                +
+              </td>
+              <td id="break-length">{this.state.breakLength}</td>
+              <td
+                id="break-decrement"
+                onClick={this.breakHandler.bind(this)}
+                className="interactive">
+                -
+              </td>
+            </tr>
+            <tr>
+              <td id="session-label" colSpan="3">
+                Session Length
+              </td>
+            </tr>
+            <tr>
+              <td
+                id="session-increment"
+                onClick={this.sessionHandler.bind(this)}
+                className="interactive">
+                +
+              </td>
+              <td id="session-length">{this.state.sessionLength}</td>
+              <td
+                id="session-decrement"
+                onClick={this.sessionHandler.bind(this)}
+                className="interactive">
+                -
+              </td>
+            </tr>
+            <tr>
+              <td id="timer-label" colSpan="3">
+                {this.state.label}
+              </td>
+            </tr>
+            <tr>
+              <td id="time-left" colSpan="3">
+                {this.state.formatted}
+              </td>
+            </tr>
+            <tr>
+              <td id="reset" onClick={this.reset} className="interactive">
+                Reset
+              </td>
+              <td
+                id="start_stop"
+                colSpan="2"
+                onClick={this.startStop}
+                className="interactive">
+                {this.state.startStop}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
